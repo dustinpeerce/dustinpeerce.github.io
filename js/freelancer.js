491e99fe2c34abc4ede99c3415155f7f05d6355a
Dustin Peerce <dustin.a.peerce@gmail.com>
2017-09-09 00:01:50 -0500

    reduced code and trimmed page

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,17 +1,12 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+function clearform() { var frm = document.getElementById('contactForm'); frm.submit(); frm.reset(); return false; }
+
+// jQuery Easing - page scrolling
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    $('.page-scroll a').bind('click', function(e) {
+        var a = $(this);
+        $('html, body').stop().animate({scrollTop: $(a.attr('href')).offset().top}, 1500, 'easeInOutExpo');
+        e.preventDefault();
     });
 });
 
@@ -27,11 +22,27 @@ $(function() {
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+$('body').scrollspy({target: '.navbar-fixed-top'});
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+$('.navbar-collapse ul li a').click(function() {$('.navbar-toggle:visible').click();});
+
+var cbpAnimatedHeader = (function() {
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-fixed-top' ),
+        didScroll = false,
+        changeHeaderOn = 300;
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {didScroll = true; setTimeout( scrollPage, 250 );}
+        }, false );
+    }
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) classie.add( header, 'navbar-shrink' );
+        else classie.remove( header, 'navbar-shrink' );
+        didScroll = false;
+    }
+    function scrollY() {return window.pageYOffset || docElem.scrollTop;}
+    init();
+})();
